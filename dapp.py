6d2bf7a3d0b378590a791137e12a02ac1c704c8d
Lyno Ferraz <lynoFerraz@gmail.com>
2023-11-06 21:08:49 -0300

    feat: Zk barretenberg verifier in Cartesi ROllups DApp

---
+++ dapp.py
@@ -0,0 +1,63 @@
+from os import environ
+import logging
+import requests
+import json
+
+from verifier import Verifier
+
+logging.basicConfig(level="INFO")
+logger = logging.getLogger(__name__)
+
+rollup_server = environ["ROLLUP_HTTP_SERVER_URL"]
+logger.info(f"HTTP rollup_server url is {rollup_server}")
+
+zk_verifier = Verifier(circuit_filepath='circuit.json', verbose=False, backend_path='/opt/backend', base_filepath='circuit')
+
+def hex2str(hexstr):
+    return bytes.fromhex(hexstr[2:]).decode("utf-8")
+
+def handle_advance(data):
+    logger.info(f"Received advance request data {data}")
+
+    try:
+        payload_json = json.loads(hex2str(data["payload"]))
+        result = zk_verifier.verify(payload_json)
+        logger.info(f"ZK Verification = {result}")
+    except Exception as e:
+        logger.error(e)
+        return "reject"
+
+    return "accept"
+
+
+def handle_inspect(data):
+    logger.info(f"Received inspect request data {data}")
+    try:
+        payload_json = json.loads(hex2str(data["payload"]))
+        result = zk_verifier.verify(payload_json)
+        logger.info(f"ZK Verification = {result}")
+    except Exception as e:
+        logger.error(e)
+        return "reject"
+
+    return "accept"
+
+
+handlers = {
+    "advance_state": handle_advance,
+    "inspect_state": handle_inspect,
+}
+
+finish = {"status": "accept"}
+
+while True:
+    logger.info("Sending finish")
+    response = requests.post(rollup_server + "/finish", json=finish)
+    logger.info(f"Received finish status {response.status_code}")
+    if response.status_code == 202:
+        logger.info("No pending rollup request, trying again")
+    else:
+        rollup_request = response.json()
+        data = rollup_request["data"]
+        handler = handlers[rollup_request["request_type"]]
+        finish["status"] = handler(rollup_request["data"])
